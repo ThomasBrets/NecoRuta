f5835e7d5cdbe629088e46281ad12847c5279889
ThomasBrets <thomas15501331@gmail.com>
2024-02-10 11:41:12 -0300

    first context

---
+++ src/context/transportContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+// Data
+import { transportData } from "../../data";
+
+// Create context
+export const transportContext = createContext();
+
+const transportProvider = ({ children }) => {
+  const [transport, setTransport] = useState(transportData);
+
+  return (
+    <transportContext.Provider value={{ transport }}>
+      {children}
+    </transportContext.Provider>
+  );
+};
